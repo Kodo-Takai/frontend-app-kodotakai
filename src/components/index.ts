37ab88eb7db98e0d4074ce5e50063b841f2e1de5
Kenith Guanilo <kenithguanilo@gmail.com>
2025-09-09 17:34:20 -0500

    refactor: remove authentication components and related styles

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// Component exports
-export { default as SplashScreen } from './SplashScreen';
-export { default as Login } from './Login';
-
-// Hook exports
-export { useSplash } from '../hooks/useSplash';
