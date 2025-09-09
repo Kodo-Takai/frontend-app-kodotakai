f7bea9889f8e8b3d444213e91e928a9b3c4944aa
AnthonyCod <englishlife61@gmail.com>
2025-09-08 21:22:03 -0500

    feat: Add Login structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import AuthLayout from './components/AuthLayout';
 import Login from './pages/Login';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<div style={{ padding: 24 }}>Recuperar contraseña</div>} />
-        <Route path="/register" element={<div style={{ padding: 24 }}>Registro</div>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <AuthLayout>
+        <Login />
+      </AuthLayout>
+    </div>
   );
 }
