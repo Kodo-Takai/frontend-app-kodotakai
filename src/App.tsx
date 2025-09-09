6be357bfba7728f3afe03a86636544c52b8bcf23
AnthonyCod <englishlife61@gmail.com>
2025-09-08 21:25:45 -0500

    feat: Clear App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import AuthLayout from './components/AuthLayout';
-import Login from './pages/Login';
-
 export default function App() {
   return (
     <div>
-      <AuthLayout>
-        <Login />
-      </AuthLayout>
+      
     </div>
   );
 }
