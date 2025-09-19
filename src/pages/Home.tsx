2ea0b008fb5503688b60c642c0d995a0bd7bdc76
Kenith Guanilo <kenithguanilo@gmail.com>
2025-09-19 18:46:38 -0500

    feat: Implement Search component with input handling and search functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,32 @@
- interface HomeProps {
-    user: {
-      name: string;
-    };
-  }
+import { FaBell } from "react-icons/fa";
+import Search from "../components/ui/search/search";
 
-  const user: HomeProps['user'] = {
-    name: '',
+interface HomeProps {
+  user: {
+    name: string;
   };
+}
+
+const user: HomeProps["user"] = {
+  name: "",
+};
 export default function Home() {
- 
   const userName = user?.name?.trim() || "Nombre y apellido";
 
   return (
-    <div className="flex max-w-md mx-auto p-6 bg-white justify-center items-center">
-      <div className="flex flex-col items-start w-full">
-        <h1 className="font-semibold text-md text-left">Hola,</h1>
-        <span className="font-extrabold text-2xl text-red-600">
-          {userName}
-        </span>
+    <div className="flex flex-col gap-4 max-w-md mx-auto p-6 bg-white justify-center items-center">
+      <div className="flex flex-row justify-between w-full">
+        <div className="flex flex-col items-start w-full">
+          <h1 className="font-semibold text-md text-left">Hola,</h1>
+          <span className="font-extrabold text-xl text-red-600">
+            {userName}
+          </span>
+        </div>
+        <div className="bg-white relative mt-6 border border-gray-600 rounded-lg p-2 shadow-sm">
+          <FaBell className=" text-gray-600" size={15} />
+        </div>
       </div>
+      <Search />
     </div>
   );
 }
