4f1e93eb4c50aed0d216ca89d0049476dcabf62c
xavierdev25 <xavierdev25@icloud.com>
2025-10-05 20:04:48 -0500

    refactor: Corrección de nombres de archivos y mejora en la gestión de autenticación
    - Se corrigió el nombre del archivo AuthLayout a authLayout.
    - Se ajustó la importación de BottomNav a bottomNav.
    - Se mejoró la lógica de acceso a los datos en el hook useAuth, asegurando que se acceda correctamente a result.data.
    - Se creó un objeto básico para userData en caso de que no se reciba información del usuario en la respuesta de inicio de sesión.
    - Se agregó manejo de errores en el hook useAuth para registrar errores de inicio de sesión.

--- src/components/layout/protectedRoute/index.tsx
+++ src/components/layout/protectedRoute/index.tsx
@@ -5,7 +5,6 @@ import type { RootState } from "../../../redux/store";
 const ProtectedRoute = () => {
   const token = useSelector((state: RootState) => state.auth.token);
   return token ? <Outlet /> : <Navigate to="/login" replace />;
-
 };
 
 export default ProtectedRoute;
