3b9706bfff81ad8943f34fad8471dbc34dc0302b
xavierdev25 <xavierdev25@icloud.com>
2025-10-07 18:24:48 -0500

    refactor: Remove unused state for hover effect in Profile component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import type { RootState } from "../redux/store";
 import { useGetProfilesQuery } from "../redux/api/profileApi";
 import { logout } from "../redux/slice/authSlice";
@@ -12,7 +12,6 @@ export default function Profile() {
   const token = useSelector((s: RootState) => s.auth.token);
   const payload = useMemo(() => decodeJwt(token), [token]);
   const profileIdFromToken = payload?.profileId as string | undefined;
-  const [isHovered, setIsHovered] = useState(false);
 
   const { data: profiles, isFetching } = useGetProfilesQuery();
 
