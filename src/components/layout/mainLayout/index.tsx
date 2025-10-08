93ed92ce58e21a25bad8939a48e5cf4f2dd0b8fc
dachugo <angelcampospezo7@outlook.es>
2025-10-08 00:08:56 -0500

    fixed: main padding

--- src/components/layout/mainLayout/index.tsx
+++ src/components/layout/mainLayout/index.tsx
@@ -4,7 +4,7 @@ import { BottomNav, defaultItems } from "../bottomNav";
 const MainLayout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-grow bg-gray-100 p-4">
+      <main className="flex-grow bg-gray-100">
         <Outlet />
       </main>
       <BottomNav items={defaultItems} />
