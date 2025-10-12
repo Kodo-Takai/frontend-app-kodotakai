85a24de9277bff30077420e466edea4e02108038
dachugo <angelcampospezo7@outlook.es>
2025-10-12 14:56:06 -0500

    fixed: ModalMove Agenda

--- src/components/ui/moveDestinationModal/MoveDestinationModal.tsx
+++ src/components/ui/moveDestinationModal/MoveDestinationModal.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
-import { es } from 'date-fns/locale';
 import type { AgendaItem } from '../../../redux/slice/agendaSlice';
 
 interface MoveDestinationModalProps {
