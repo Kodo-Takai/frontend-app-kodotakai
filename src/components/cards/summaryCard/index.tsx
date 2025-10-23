15f1f5e915503382cf600d6a2340e8bac8abdafb
dachugo <angelcampospezo7@outlook.es>
2025-10-22 22:53:36 -0500

    added: SummaryCards animations

--- src/components/cards/summaryCard/index.tsx
+++ src/components/cards/summaryCard/index.tsx
@@ -10,13 +10,13 @@ export default function SummaryCard() {
       </h1>
       <div className="grid grid-cols-5 gap-2 h-32">
         <div className="col-span-3 h-full">
-          <MapsCard className="w-full h-full" />
+          <MapsCard className="w-full h-full animate-bubble-in" />
         </div>
         <div className="col-span-2 flex flex-col gap-5 h-full">
-          <div className="flex-[3]">
-            <WeatherCard className="w-full h-full" />
+          <div className="flex-[3] ">
+            <WeatherCard className="w-full h-full animate-bubble-in" />
           </div>
-          <div className="flex-[2] relative z-20">
+          <div className="flex-[2] relative z-20 animate-bubble-in">
             <CalendarCard />
           </div>
         </div>
